3a599ee88ee2d7e1d86f26f38fafd2f183b5b19f
William Dueease <wdueease@altamusinc.com>
2024-10-24 17:32:12 -0400

    add clear eeprom message

--- src/mavlink/v2.0/altamus/altamus.h
+++ src/mavlink/v2.0/altamus/altamus.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ALTAMUS.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ALTAMUS_XML_HASH -2725825896179079477
+#define MAVLINK_ALTAMUS_XML_HASH -3961088742753143387
 
 #ifdef __cplusplus
 extern "C" {
