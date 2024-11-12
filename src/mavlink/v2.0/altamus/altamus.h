087c9c9e8bdb36548478d54bc8a36081b9d55934
William Dueease <wdueease@altamusinc.com>
2024-11-12 17:29:52 -0500

    add invalid readings for lat/lon

--- src/mavlink/v2.0/altamus/altamus.h
+++ src/mavlink/v2.0/altamus/altamus.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ALTAMUS.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ALTAMUS_XML_HASH 1006497395967906821
+#define MAVLINK_ALTAMUS_XML_HASH -6378608181707318773
 
 #ifdef __cplusplus
 extern "C" {
--- src/mavlink/v2.0/common_minimal/common_minimal.h
+++ src/mavlink/v2.0/common_minimal/common_minimal.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_COMMON_MINIMAL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_COMMON_MINIMAL_XML_HASH -3770652590039435947
+#define MAVLINK_COMMON_MINIMAL_XML_HASH -855838286826535809
 
 #ifdef __cplusplus
 extern "C" {
