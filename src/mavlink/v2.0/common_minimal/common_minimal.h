7a1a7448cfa4929754c93b3efab29072e8766c5b
William Dueease <wdueease@altamusinc.com>
2025-08-11 17:01:01 -0400

    errant comma was breaking dart mavlink maker

--- src/mavlink/v2.0/common_minimal/common_minimal.h
+++ src/mavlink/v2.0/common_minimal/common_minimal.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_COMMON_MINIMAL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_COMMON_MINIMAL_XML_HASH -6555301819536351337
+#define MAVLINK_COMMON_MINIMAL_XML_HASH -443562985679739457
 
 #ifdef __cplusplus
 extern "C" {
