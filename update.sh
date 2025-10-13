fedd2cab810a8f62ebfcea9bf7491790476c45d1
William Dueease <wdueease@altamusinc.com>
2025-10-13 09:26:22 -0400

    update scan transform with max range and range scale, add super simple update script to try to consolidate mavlink updates to various repos

---
+++ update.sh
@@ -0,0 +1,6 @@
+source ./.venv/bin/activate
+python -m pymavlink.tools.mavgen --lang=C --wire-protocol=2.0 --output=src/mavlink/v2.0 message_definitions/v1.0/altamus.xml
+python -m pymavlink.tools.mavgen --lang=WLua --wire-protocol=2.0 --output=src/mavlink/altamus_mavlink message_definitions/v1.0/altamus.xml
+cd altamus_mavlink_dart
+git submodule update --remote
+dart run ./tool/generate.dart -d mavlink/message_definitions/v1.0/altamus.xml
